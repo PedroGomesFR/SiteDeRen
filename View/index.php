54e25b7deb5d740d5b39396ac5e76f68adbd3316
PedroGomes <113339904+PedroGomesFR@users.noreply.github.com>
2024-11-07 22:30:10 +0100

    new

---
+++ View/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Rencontre Plus</title>
+    <!-- couleur rose : #E44A62  couleur noir/gris : #343434  -->
+</head>
+<body>
+    <?php include('./templates/header.php')?>
+
+
+
+
+
+<!-- <?php include('./templates/footer.php')?> -->
+
+</body>
+</html>
---
+++ View/templates/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+
+        <title>Document</title>
+    </head>
+    <body>
+
+        <header>
+            <div class="header-container">
+                <h1>Rencontre plus</h1>
+
+                <div class="liens">
+                    <a href="">Connexion/Inscription</a>
+                    <a href="">Annonce</a>
+                    <a href="">Recherche</a>
+                    <a href="">Likes</a>
+                </div>
+            </div>
+        </header>
+
+    </body>
+</html>
