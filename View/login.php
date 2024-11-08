96fbf83dab6c55dfb6b37f5b1d4948b0744a4f44
PedroGomesFR <113339904+PedroGomesFR@users.noreply.github.com>
2024-11-08 15:37:19 +0100

    new

---
+++ View/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>RP</title>
+    </head>
+    <body>
+
+        <div class="centre-wrapper">
+            <div class="containerCon">
+                <form action="Connexion">
+                    <h1>Connexion</h1>
+                    <input type="text" name="Email" placeholder="Email">
+                </form>
+            </div>
+        </div>
+        <form action="index.php" method="post"></body>
+    </html>
