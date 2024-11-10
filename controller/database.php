3506c4c6f8de2fec4f31eda31794c805baa02878
PedroGomesFR <113339904+PedroGomesFR@users.noreply.github.com>
2024-11-10 20:17:10 +0100

    new

--- controller/database.php
+++ controller/database.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
     private $host = "localhost";
-    private $db_name = "mon_projet";
+    private $db_name = "rencontreplus";
     private $username = "root"; 
     private $password = "";
     public $conn;
